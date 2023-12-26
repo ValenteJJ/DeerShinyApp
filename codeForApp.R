a36c5895092a722a2c0de783ea64b57fc40d674b
Jonathon J. Valente <Jonathon.J.Valente@gmail.com>
2023-12-26 09:14:42 -0600

    Small change

--- codeForApp.R
+++ codeForApp.R
@@ -71,3 +71,7 @@ forestNon = classify(croppedNLCD, rcl=reclassifyValues)
 
 #Now we can look at the distribution of forest (1 values) in the county
 plot(forestNon)
+
+
+
+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
